effec71827c99379091a9be28c54283fdf073526
Emre Akyüz <aemreakyuz@gmail.com>
2023-10-19 21:39:47 +0300

    success page düzenlendi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import Header from "../layout/Header";
 import "../layout/Header.css";
 import React from "react";
+import Footer from "../layout/Footer.js";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
--- src/pages/SuccessPage.js
+++ src/pages/SuccessPage.js
@@ -12,35 +12,38 @@ const SuccessPage = (props) => {
         <hr className="space" />
         <h4>lezzetin yolda</h4>
         <h2>{order ? "SİPARİŞ ALINDI" : "SİPARİŞ BULUNAMADI"}</h2>
-      </div>
-      <hr className="separator" />
-      <div className="order-summary">
-        <h3>{order ? order.title : "Sipariş Bulunamadı"}</h3>
-        <div className="space" />
-        <div className="order-info">
-          <p>
-            Boyut: <span>{order ? order.size : "Sipariş Bulunamadı"}</span>
-          </p>
-          <p>
-            Hamur: <span> {order ? order.hamur : "Sipariş Bulunamadı"}</span>
-          </p>
-          <p className="ek-malzeme">
-            Ek Malzemeler:
-            <span>
-              {order ? order.ekstraMalzemeler.join(", ") : "Sipariş Bulunamadı"}
-            </span>
-          </p>
-        </div>
-        <hr className="space" />
-        <div className="order-price-container">
-          <h3>Sipariş Toplamı</h3>
-          <div className="ekmalzemeler-price">
-            <h4>Seçimler</h4>
-            <h4>{order ? order.ekstraMalzemelerFiyat : "0"}₺ </h4>
+
+        <hr className="separator" />
+        <div className="order-summary">
+          <h3>{order ? order.title : "Sipariş Bulunamadı"}</h3>
+          <div className="space" />
+          <div className="order-info">
+            <p>
+              Boyut: <span>{order ? order.size : "Sipariş Bulunamadı"}</span>
+            </p>
+            <p>
+              Hamur: <span> {order ? order.hamur : "Sipariş Bulunamadı"}</span>
+            </p>
+            <p className="ek-malzeme">
+              Ek Malzemeler:
+              <span>
+                {order
+                  ? order.ekstraMalzemeler.join(", ")
+                  : "Sipariş Bulunamadı"}
+              </span>
+            </p>
           </div>
-          <div className="total-price">
-            <h4>Toplam</h4>
-            <h4>{order ? order.totalPrice : 0}₺</h4>
+          <hr className="space" />
+          <div className="order-price-container">
+            <h3>Sipariş Toplamı</h3>
+            <div className="ekmalzemeler-price">
+              <h4>Seçimler</h4>
+              <h4>{order ? order.ekstraMalzemelerFiyat : "0"}₺ </h4>
+            </div>
+            <div className="total-price">
+              <h4>Toplam</h4>
+              <h4>{order ? order.totalPrice : 0}₺</h4>
+            </div>
           </div>
         </div>
       </div>
